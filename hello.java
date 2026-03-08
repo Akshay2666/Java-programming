28f4dc8f42078e4ca01e0a911387aa2d78da240a
Akshay Mahangade <akshaymahangade2666@gmail.com>
2026-03-08 11:59:51 +0530

    addition perform

--- hello.java
+++ hello.java
@@ -3,5 +3,8 @@ package Java-programming;
  class hello {
    public static void main(String[] args) {
     System.out.print("hello");
+    int a=10;
+    int b=20;
+    System.out.print(a+b);
    } 
 }
