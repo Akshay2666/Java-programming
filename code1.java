143ac704995cfc60f05261a4a58feb47d6ca6421
Akshay Mahangade <akshaymahangade2666@gmail.com>
2026-03-10 10:10:34 +0530

    adding three num

--- code1.java
+++ code1.java
@@ -1,5 +1,7 @@
-public class code1 {
+public class code1{
     public static void main(String[] args) {
-        System.out.println("bsdk teri mkc");
+        int a=10, b=20, c=20;
+        int res= a+b+c;
+        System.out.println(res);
     }
 }
