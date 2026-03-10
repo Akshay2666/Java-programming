0211646c23afe44e38b75606337da08446fa51b1
Akshay Mahangade <akshaymahangade2666@gmail.com>
2026-03-10 23:51:38 +0530

    sum of 3 num

---
+++ Program2.java
@@ -0,0 +1,10 @@
+public class Program2 {
+    public static void main(String[] args) {
+        
+        int a=10, b=20, c=20;
+        int sum=a+b+c;
+
+        System.out.println("Your sum is:"+sum);
+    }
+    
+}
