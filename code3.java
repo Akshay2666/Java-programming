3fb56f8dee86093987cc9611c17151570a86323c
Akshay Mahangade <akshaymahangade2666@gmail.com>
2026-03-10 23:12:38 +0530

    greeting message

---
+++ code3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class code3 {
+    public static void main(String[] args) {
+      System.out.println("Enter your name");
+      Scanner sc =new Scanner(System.in);
+      String name =sc.next();
+      
+      System.out.println("hello"+" "+name+" "+"have a good day");
+    
+}
+}
