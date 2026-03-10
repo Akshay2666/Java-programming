c21da512e647bf9bee581c2f02cc3cc3bdd4bad4
Akshay Mahangade <akshaymahangade2666@gmail.com>
2026-03-10 23:42:44 +0530

    cgpa calculation

---
+++ Program1.java
@@ -0,0 +1,14 @@
+public class Program1 {
+    public static void main(String[] args) {
+        
+    
+    
+        float a=97.0f, b=95.0f, c=90.0f;
+
+        float res= (a+b+c)/30;
+        System.out.println("Your cgpa is:"+res);
+    
+    
+    }
+    
+}
