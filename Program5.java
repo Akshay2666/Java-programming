6a22d4cf5292be7f5f6903d1269ae7fbf43bfdbc
Akshay Mahangade <akshaymahangade2666@gmail.com>
2026-03-12 00:02:30 +0530

    num checking

---
+++ Program5.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Program5 {
+    public static void main(String[] args) {
+       System.out.println("Enter any number"); 
+       Scanner sc=new Scanner(System.in);
+       boolean a=sc.hasNextLine();
+       System.out.println(a);
+    }
+}
