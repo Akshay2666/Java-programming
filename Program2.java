b6adcd3b708e1f4b32fdbf9b26821ce920105a2f
Shubham-lk-codes <Shubhamlonkar137@gmail.com>
2026-03-12 22:31:19 +0530

    program2.java updated for 4 num sum

--- Program2.java
+++ Program2.java
@@ -1,8 +1,8 @@
 public class Program2 {
     public static void main(String[] args) {
         
-        int a=10, b=20, c=20;
-        int sum=a+b+c;
+        int a=10, b=20, c=20, d=30;
+        int sum=a+b+c+d;
 
         System.out.println("Your sum is:"+sum);
     }
