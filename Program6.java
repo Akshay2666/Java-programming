0c9b38e41b87534f7b7a7b966fb4015645b32a55
Akshay Mahangade <akshaymahangade2666@gmail.com>
2026-03-12 19:20:17 +0530

    comparison code

---
+++ Program6.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Program6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number");
+        int a=sc.nextInt();
+        int b=25;
+        System.out.println(a>b);
+        
+    }
+}
