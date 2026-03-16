ee4622fa4d19670f540b4fc64edd085b827f9cb9
Akshay Mahangade <akshaymahangade2666@gmail.com>
2026-03-17 00:05:46 +0530

    escape sequence

---
+++ Program12.java
@@ -0,0 +1,6 @@
+public class Program12 {
+    public static void main(String[] args) {
+        String name="Dear akshay ,\n this java course is nice.\n Thanks.";
+        System.out.println(name);
+    }
+}
