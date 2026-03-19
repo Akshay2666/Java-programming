587f2a75318dcf001548141c58a593816b88ed46
Akshay Mahangade <akshaymahangade2666@gmail.com>
2026-03-20 00:28:19 +0530

    loop f5q

---
+++ Program16.java
@@ -0,0 +1,24 @@
+public class Program16 {
+    public static void main(String[] args) {
+        
+        //Code 1-print num 1 to 10
+         for(int i=1;i<=10;i++){
+            System.out.println(i);
+         }
+
+         //Code 2-print num 10 to 1
+         for(int i=10;i>0;i--){
+            System.out.println(i);
+         }
+
+         //Code 3-print even numbers 1 to 20
+         for(int i=2;i<=20;i+=2){
+System.out.println(i);
+         }
+
+         //Code 4-print odd numbers 1 to 20
+         for(int i=1;i<=20;i+=2){
+            System.out.println(i);
+         }
+    }
+}
