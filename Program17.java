ecfb9a8ce44e9a6d6708984a2dbe80ac11003b8b
Akshay Mahangade <akshaymahangade2666@gmail.com>
2026-03-26 00:02:28 +0530

    loop 10 q

---
+++ Program17.java
@@ -0,0 +1,28 @@
+public class Program17 {
+    public static void main(String[] args) {
+        //code 6
+        for(int i=1;i<=10;i++){
+            System.out.println(i*i);
+        }
+        //code 7
+        for(int i=1;i<=10;i++){
+        System.out.println(i*i*i);
+        }
+        //code 8
+        for(int i=1;i<=10;i++){
+            System.out.println(5*i);
+        }
+        //code 9
+        for(int i=1;i<=30;i++){
+            if (i%3==0) {
+             System.out.println(i);   
+            }
+        }
+        // code 10
+        for(int i=1;i<=50;i++){
+            if (i%5==0) {
+               System.out.println(i); 
+            }
+        }
+    }
+}
