451afdedc7a500d2bee1142e9846a8f30ffa2675
Akshay Mahangade <akshaymahangade2666@gmail.com>
2026-03-28 01:03:17 +0530

    15q

---
+++ Program18.java
@@ -0,0 +1,39 @@
+public class Program18 {
+    public static void main(String[] args) {
+        //Code 11
+        /*int sum=0;
+        for(int i=1;i<=10;i++){
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
+       //Code 12
+       int sum=0;
+       for(int i=2;i<=20;i+=2){
+        sum+=i;
+       }
+       System.out.println(sum);
+}
+       //Code 13
+       int sum=0;
+       for(int i=1;i<=20;i+=2){
+        sum+=i;
+       }
+       System.out.println(sum);
+}
+       //Code 14
+       int sum=0;
+       for(int i=1;i<=10;i++){
+        sum+=(i*i);
+       }
+       System.out.println(sum);
+    }
+       //Code 15
+        int sum=0;
+       for(int i=1;i<=10;i++){
+        sum+=(i*i*i);
+       }
+       System.out.println(sum);
+    }*/
+}
+}
